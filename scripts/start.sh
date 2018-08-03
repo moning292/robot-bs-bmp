fd246c1c2328e9be70802a48028710a8dc8a8c49
Worakorn Chaichakan <worakorn.chaichakan@news.com.au>
2018-08-03 16:19:26 +1000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,17 +6,17 @@ export TTL
 export BS_ARGS
 export BS_KEY
 
-wait_until(){
-        #waits untill the proxy server doesn't comes up
-        while [ 1 -eq 1 ];
-        do
-                echo "Waiting for proxy to come up ....."
-                resp="curl -X GET 'http://localhost:$BMP_PORT/proxy/' 2>/dev/null"
-                echo $resp | grep 'proxyList' 1>/dev/null
-                [ $? -eq 0 ] && echo "Proxy is up now ! " && break
-                sleep 5
-        done
-}
+# wait_until(){
+#         #waits untill the proxy server doesn't comes up
+#         while [ 1 -eq 1 ];
+#         do
+#                 echo "Waiting for proxy to come up ....."
+#                 resp="curl -X GET 'http://localhost:$BMP_PORT/proxy/' 2>/dev/null"
+#                 echo $resp | grep 'proxyList' 1>/dev/null
+#                 [ $? -eq 0 ] && echo "Proxy is up now ! " && break
+#                 sleep 5
+#         done
+# }
 
 echo "BMP port is $BMP_PORT"
 echo "Port Range is $PORT_RANGE"
@@ -24,7 +24,7 @@ echo "TTL is $TTL"
 
 /browsermob-proxy/bin/browsermob-proxy -port $BMP_PORT -proxyPortRange $PORT_RANGE -ttl $TTL &
 
-wait_until
+# wait_until
 
 echo "BrowserStack argument is $BS_ARGS"
 echo "BrowserStack key is $BS_KEY"
