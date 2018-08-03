9497713bbe33bf320cce78025a0f8e115e093f11
Worakorn Chaichakan <worakorn.chaichakan@news.com.au>
2018-08-03 15:53:36 +1000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,10 +10,10 @@ wait_until(){
         #waits untill the proxy server doesn't comes up
         while [ 1 -eq 1 ];
         do
-                echo 'Waiting for proxy to come up .....'
-                resp=`curl -X GET 'http://localhost:' && $BMP_PORT && '/proxy/' 2>/dev/null`
+                echo "Waiting for proxy to come up ....."
+                resp="curl -X GET 'http://localhost:$BMP_PORT/proxy/' 2>/dev/null"
                 echo $resp | grep 'proxyList' 1>/dev/null
-                [ $? -eq 0 ] && echo 'Proxy is up now ! ' && break
+                [ $? -eq 0 ] && echo "Proxy is up now ! " && break
                 sleep 5
         done
 }
@@ -22,13 +22,13 @@ echo "BMP port is $BMP_PORT"
 echo "Port Range is $PORT_RANGE"
 echo "TTL is $TTL"
 
-/browsermob-proxy/bin/browsermob-proxy -port $BMP_PORT -proxyPortRange $PORT_RANGE -ttl $TTL
+/browsermob-proxy/bin/browsermob-proxy -port $BMP_PORT -proxyPortRange $PORT_RANGE -ttl $TTL &
 
 wait_until
 
 echo "BrowserStack argument is $BS_ARGS"
 echo "BrowserStack key is $BS_KEY"
 
-/usr/local/bin/BrowserStackLocal $BS_ARGS $BS_KEY
+/usr/local/bin/BrowserStackLocal $BS_ARGS $BS_KEY &
 
 echo "BrowserStackLocal is starting..."
